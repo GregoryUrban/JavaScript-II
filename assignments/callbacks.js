6bbbaa912ad7c2cb974f446e37507fa9c6612fc2
GregoryUrban <turknben@gmail.com>
2019-01-18 00:21:55 -0500

    Callbacks.js done except last one

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -2,8 +2,8 @@
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
-/* 
 
+/*
   //Given this problem: 
   
   function firstItem(arr, cb) {
@@ -13,30 +13,53 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
   // Potential Solution:
   function firstItem(arr, cb) {
     return cb(arr[0]);
-  }
+  };
 
   firstItem(items, function(first) {
-    console.log(first)
+    console.log(first);
   });
-
 */
 
 
+
 function getLength(arr, cb) {
+  return cb(arr.length);
   // getLength passes the length of the array into the callback.
 }
+getLength(items, function(lengf) {
+  console.log(lengf);
+});
+
+// console.log(getLength(items));
 
 function last(arr, cb) {
+  return cb(arr.slice(-1));
   // last passes the last item of the array into the callback.
 }
+last(items, function(end) {
+  console.log(end);
+});
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return (x + y);
 }
+sumNums(items, function(sums) {
+  console.log(sums);
+});
+const result = sumNums(5,7);
+console.log(result);
+
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return (x * y);
 }
+multiplyNums(items, function(multi) {
+  console.log(multi);
+});
+const results = multiplyNums(5,7);
+console.log(results);
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
